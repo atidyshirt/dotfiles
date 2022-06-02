658a7cc1c1e87da5bf76c2f88767c08141d23668
Jordan Pyott <jordanpyott@gmail.com>
2022-06-02 20:24:20 +1200

    MAINT: adding linux dotfiles to stow

---
+++ .config/i3/bin/autostart.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+## Autostart Programs
+
+# Capslock mapped to escape
+xmodmap -e 'clear Lock' #ensures you're not stuck in CAPS on mode
+xmodmap -e 'keycode 0x42=Escape' #remaps the keyboard so CAPS LOCK=ESC
+
+# Kill already running process
+_ps=(picom dunst ksuperkey mpd xfce-polkit xfce4-power-manager)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Polkit agent
+/usr/lib/xfce-polkit/xfce-polkit &
+
+# Enable power management
+xfce4-power-manager &
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
+
+# hide cursor when inactive
+unclutter &
+
+# Restore wallpaper
+# hsetroot -cover ~/.config/i3/wallpapers/home_gruvbox_comfy_brown.jpg
+nitrogen --restore
+
+# Lauch notification daemon
+~/.config/i3/bin/i3dunst.sh
+
+# Lauch polybar
+~/.config/i3/bin/i3bar.sh
+
+# Lauch compositor
+~/.config/i3/bin/i3comp.sh
