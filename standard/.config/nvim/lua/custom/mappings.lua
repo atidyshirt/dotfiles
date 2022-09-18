cae9f5f1ff021dc325ccb4ede51c268be99f48ed
Jordan Pyott <jordanpyott@gmail.com>
2022-09-18 18:40:56 +1200

    MAINT: updating dotfiles to support harpoon cmd

--- standard/.config/nvim/lua/custom/mappings.lua
+++ standard/.config/nvim/lua/custom/mappings.lua
@@ -21,6 +21,15 @@ M.general = {
       end,
       "lsp rename",
     },
+    -- Harpoon
+    ["ga"] = { ":lua require('harpoon.mark').add_file()<CR>" },
+    ["gm"] = { ":lua require('harpoon.ui').toggle_quick_menu()<CR>" },
+    ["gj"] = { ":lua require('harpoon.ui').nav_file(1)<CR>" },
+    ["gk"] = { ":lua require('harpoon.ui').nav_file(2)<CR>" },
+    ["gl"] = { ":lua require('harpoon.ui').nav_file(3)<CR>" },
+    ["g;"] = { ":lua require('harpoon.ui').nav_file(4)<CR>" },
+    ["<leader><space>m"] = { ":lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>" },
+    ["<leader><space>p"] = { ":!pandoc % -o %<.pdf --from markdown --template eisvogel --listings --toc<CR>" },
   },
   v = {
     ['<'] = { '<gv', opts = {noremap = true, silent = false} },
@@ -35,16 +44,6 @@ local function map(mode, bind, exec, opts)
     vim.api.nvim_set_keymap(mode, bind, exec, opts)
 end
 
--- {{{ Harpoon bindings
-map("n", "ga", ":lua require('harpoon.mark').add_file()<CR>", {})
-map("n", "gm", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", {})
-map("n", "gj", ":lua require('harpoon.ui').nav_file(1)<CR>", {})
-map("n", "gk", ":lua require('harpoon.ui').nav_file(2)<CR>", {})
-map("n", "gl", ":lua require('harpoon.ui').nav_file(3)<CR>", {})
-map("n", "g;", ":lua require('harpoon.ui').nav_file(4)<CR>", {})
-map("n", "<leader><space>m", ":lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>", {})
-map("n", "<leader><space>p", ":!pandoc % -o %<.pdf --from markdown --template eisvogel --listings --toc<CR>", {})
--- }}}
 local in_tmux_session = vim.fn.system('echo $TMUX'):find('tmux')
 if in_tmux_session then
 		map("n", "gt", ":lua require('harpoon.tmux').gotoTerminal(1)<CR>", {})
