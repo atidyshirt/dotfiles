fc48f16d2c8cfef7dc0c83c98f89ceca782873c3
jordanp <jordanpyott@gmail.com>
2022-11-27 23:35:27 +1300

    MAINT: removing changing of border color from sketchybar config

--- mac/.config/sketchybar/plugins/yabai.sh
+++ mac/.config/sketchybar/plugins/yabai.sh
@@ -11,7 +11,7 @@ window_state() {
   if [[ $CURRENT -gt 0 ]]; then
     LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
     args+=(--set $NAME icon=$YABAI_STACK icon.color=$ACCENT label.drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST"))
-    yabai -m config active_window_border_color $ACCENT > /dev/null 2>&1 &
+    # yabai -m config active_window_border_color $ACCENT > /dev/null 2>&1 &
 
   else 
     args+=(--set $NAME label.drawing=off)
@@ -19,18 +19,18 @@ window_state() {
       "false")
         if [ "$(echo "$WINDOW" | jq '.["has-fullscreen-zoom"]')" = "true" ]; then
           args+=(--set $NAME icon=$YABAI_FULLSCREEN_ZOOM icon.color=$GREEN)
-          yabai -m config active_window_border_color $GREEN > /dev/null 2>&1 &
+          # yabai -m config active_window_border_color $GREEN > /dev/null 2>&1 &
         elif [ "$(echo "$WINDOW" | jq '.["has-parent-zoom"]')" = "true" ]; then
           args+=(--set $NAME icon=$YABAI_PARENT_ZOOM icon.color=$BLUE)
-          yabai -m config active_window_border_color $BLUE > /dev/null 2>&1 &
+          # yabai -m config active_window_border_color $BLUE > /dev/null 2>&1 &
         else
           args+=(--set $NAME icon=$YABAI_GRID icon.color=$GREEN)
-          yabai -m config active_window_border_color $ACCENT > /dev/null 2>&1 &
+          # yabai -m config active_window_border_color $ACCENT > /dev/null 2>&1 &
         fi
         ;;
       "true")
         args+=(--set $NAME icon=$YABAI_FLOAT icon.color=$ACCENT)
-        yabai -m config active_window_border_color $ACCENT > /dev/null 2>&1 &
+        # yabai -m config active_window_border_color $ACCENT > /dev/null 2>&1 &
         ;;
     esac
   fi
