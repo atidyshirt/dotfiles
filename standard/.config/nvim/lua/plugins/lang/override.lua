e5d50a0227e653a761f3d16cfa577e2d3dbd828a
jordanp <jordanpyott@gmail.com>
2023-03-09 22:10:08 +1300

    MAINT: setting up nvim-cmp to my preferences

--- standard/.config/nvim/lua/plugins/lang/override.lua
+++ standard/.config/nvim/lua/plugins/lang/override.lua
@@ -1,3 +1,8 @@
+local present, cmp = pcall(require, "cmp")
+if not present then
+    return
+end
+
 return {
     {
         "neovim/nvim-lspconfig",
@@ -53,6 +58,50 @@ return {
                     end
                 end)
             end,
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        opts = {
+            mapping = cmp.mapping.preset.insert({
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.close(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = false,
+                }),
+                ["<C-j>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif require("luasnip").expand_or_jumpable() then
+                        vim.fn.feedkeys(
+                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+                            ""
+                        )
+                    else
+                        fallback()
+                    end
+                end, {
+                    "i",
+                    "s",
+                }),
+                ["<C-k>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif require("luasnip").jumpable(-1) then
+                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                    else
+                        fallback()
+                    end
+                end, {
+                    "i",
+                    "s",
+                }),
+            }),
         },
     },
 }
