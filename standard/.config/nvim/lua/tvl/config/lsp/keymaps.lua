cf7ce5f047ecd52857350f704888bd036e289d1a
jordanp <jordanpyott@gmail.com>
2023-08-24 23:00:44 +1200

    MAINT: Fixing keybinds for harpoon (conflict)

--- standard/.config/nvim/lua/tvl/config/lsp/keymaps.lua
+++ standard/.config/nvim/lua/tvl/config/lsp/keymaps.lua
@@ -4,7 +4,7 @@ M.attach = function(client, buffer)
   local opts = { noremap = true, silent = true }
   local map = vim.api.nvim_buf_set_keymap
   map(buffer, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  map(buffer, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  map(buffer, "n", "<leader>ld", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   map(buffer, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   map(buffer, "n", "]d", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
   map(buffer, "n", "[d", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>", opts)
