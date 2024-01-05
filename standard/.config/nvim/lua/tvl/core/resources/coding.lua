79421d981f298d999f5ca69bd2914c7cfd04e8a5
jordanp <jordanpyott@gmail.com>
2024-01-05 13:18:24 +1300

    MAINT: Adding project wide QF diagnostics

--- standard/.config/nvim/lua/tvl/core/resources/coding.lua
+++ standard/.config/nvim/lua/tvl/core/resources/coding.lua
@@ -1,46 +1,5 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-        require("luasnip.loaders.from_snipmate").lazy_load()
-      end,
-    },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
-    },
-    -- stylua: ignore
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = "i",
-      },
-      {
-        "<tab>",
-        function()
-          require("luasnip").jump(1)
-        end,
-        mode = "s",
-      },
-      {
-        "<s-tab>",
-        function()
-          require("luasnip").jump(-1)
-        end,
-        mode = { "i", "s" },
-      },
-    },
-  },
-
-  {
     "Wansmer/treesj",
     keys = {
       {
@@ -132,6 +91,12 @@ return {
   },
 
   { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+  {
+    'dmmulroy/tsc.nvim',
+    config = function ()
+      require('tsc').setup()
+    end
+  },
 
   {
     "ray-x/lsp_signature.nvim",
@@ -152,12 +117,4 @@ return {
   },
 
   { "folke/neodev.nvim", opts = {} }
-
-  -- {
-  --   "jcdickinson/codeium.nvim",
-  --   event = { "InsertEnter", "CmdlineEnter" },
-  --   config = function()
-  --     require("codeium").setup({})
-  --   end,
-  -- },
 }
--- standard/.config/nvim/lua/tvl/core/resources/colorscheme.lua
+++ standard/.config/nvim/lua/tvl/core/resources/colorscheme.lua
@@ -1,8 +1,3 @@
--- disable git detecting changes
--- git update-index --skip-worktree lua/tvl/core/resources/colorscheme.lua
-
--- enable git detecting changes
--- git update-index --no-skip-worktree lua/tvl/core/resources/colorscheme.lua
 return {
   {
     "folke/tokyonight.nvim",
@@ -12,52 +7,5 @@ return {
   {
     "sainnhe/gruvbox-material",
     lazy = true,
-  },
-
-  {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      local monokai = require("monokai-pro")
-      monokai.setup({
-        transparent_background = false,
-        devicons = true,
-        filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-        day_night = {
-          enable = false,
-          day_filter = "pro",
-          night_filter = "spectrum",
-        },
-        inc_search = "background", -- underline | background
-        background_clear = { "nvim-tree", "neo-tree", "bufferline" },
-        plugins = {
-          bufferline = {
-            underline_selected = true,
-            underline_visible = false,
-            underline_fill = true,
-            bold = false,
-          },
-          indent_blankline = {
-            context_highlight = "pro", -- default | pro
-            context_start_underline = true,
-          },
-        },
-        override = function(c)
-          return {
-            ColorColumn = { bg = c.editor.background },
-            DashboardRecent = { fg = c.base.magenta },
-            DashboardProject = { fg = c.base.blue },
-            DashboardConfiguration = { fg = c.base.white },
-            DashboardSession = { fg = c.base.green },
-            DashboardLazy = { fg = c.base.cyan },
-            DashboardServer = { fg = c.base.yellow },
-            DashboardQuit = { fg = c.base.red },
-            CmpItemKindVariable = { fg = "#09B6A2" },
-          }
-        end,
-      })
-      monokai.load()
-    end,
   },
 }
