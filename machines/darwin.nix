c3f4634b0c9072e8a9faa7a1f7a9b088333c9b59
jordanp <jordanpyott@gmail.com>
2026-08-22 22:17:23 +1200

    chore: add garbage collection to darwin.nix

--- machines/darwin.nix
+++ machines/darwin.nix
@@ -25,6 +25,25 @@
     "flakes"
   ];
 
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 3;
+      Minute = 0;
+    };
+    options = "--delete-older-than 30d";
+  };
+
+  nix.optimise = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 4;
+      Minute = 0;
+    };
+  };
+
   users.users.${username} = {
     home = "/Users/${username}";
     shell = pkgs.zsh;
